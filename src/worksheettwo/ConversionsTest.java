f77363e296a07068239750f3265cc63367d84ab3
dangdang <dangdang@172.17.20.155>
2026-02-24 20:54:55 +0000

    Added failing unit test for dollarToEuro

--- src/worksheettwo/ConversionsTest.java
+++ src/worksheettwo/ConversionsTest.java
@@ -13,4 +13,14 @@ public class ConversionsTest {
         assertEquals(0.0, c.euroToDollar(0), 0.0001);
         assertEquals(-110.0, c.euroToDollar(-100), 0.0001);
     }
+
+    @Test
+    void testDollarToEuro() {
+        Conversions c = new Conversions();
+
+        assertEquals(100.0, c.dollarToEuro(110), 0.0001);
+        assertEquals(0.0, c.dollarToEuro(0), 0.0001);
+        assertEquals(-100.0, c.dollarToEuro(-110), 0.0001);
+    }
+
 }
